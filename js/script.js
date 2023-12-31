f81ab8464de91349500472156ef3063a9feb6c46
TaeSym <shauntesymone@gmail.com>
2023-12-30 20:11:22 -0500

    defined variables

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-// Your JS code here
+var subTypeElement = document.querySelector("#subscription");
+var subDurationElement = document.querySelector("#months");
+var results = document.querySelector(".result");
+var subType = "basic";
+var subDuration = 1;
